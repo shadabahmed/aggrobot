5f7216fab86e3c9d65b278cdb3de36af94539ca3
Shadab Ahmed <shadab.ansari@gmail.com>
2014-09-03 17:22:31 +0530

    Preventing connecting to db on setup

--- lib/aggrobot/sql_functions.rb
+++ lib/aggrobot/sql_functions.rb
@@ -7,11 +7,11 @@ module Aggrobot
     autoload :PgSQL, File.expand_path('../sql_functions/pgsql', __FILE__)
     autoload :SQLite, File.expand_path('../sql_functions/sqlite', __FILE__)
 
-    POSTGRES_ADAPTER_NAME = 'PostgreSQL'
-    SQLITE_ADAPTER_NAME = 'SQLite'
-    MYSQL_ADAPTER_NAME = 'Mysql2'
+    POSTGRES_ADAPTER_NAME = 'postgresql'
+    SQLITE_ADAPTER_NAME = 'sqlite3'
+    MYSQL_ADAPTER_NAME = 'mysql2'
 
-    def self.setup(precision = 2, adapter = ActiveRecord::Base.connection.adapter_name)
+    def self.setup(precision = 2, adapter = ActiveRecord::Base.configurations[Rails.env]['adapter'])
       extend Common
       self.precision = precision
       adapter_module = case adapter
